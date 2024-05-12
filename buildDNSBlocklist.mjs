795cb87be53830b301d42c78af23fb9b5591774b
bryopsida <8363252+bryopsida@users.noreply.github.com>
2024-05-12 17:07:49 -0500

    add more lists

--- buildDNSBlocklist.mjs
+++ buildDNSBlocklist.mjs
@@ -4,8 +4,18 @@ import axios from 'axios'
 
 const bigList = await axios.get('https://big.oisd.nl/dnsmasq')
 const youtube = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/pihole-youtube.txt')
+const facebook = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-facebook.txt')
+const tiktok = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-tiktok.txt')
+const reddit = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-reddit.txt')
+const twitch = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-twitch.txt')
+const snapchat = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-snapchat.txt')
+const whatsapp = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-whatsapp.txt')
+const instagram = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-instagram.txt')
+const twitter = await axios.get('https://raw.githubusercontent.com/gieljnssns/Social-media-Blocklists/master/adguard-twitter.txt')
 
-const lines = bigList.data.split('\n').concat(youtube.data.split('\n'))
+const responses = [bigList, youtube, facebook, tiktok, reddit, twitch, snapchat, whatsapp, instagram, twitter]
+
+const lines = responses.map((resp) => resp.data.split('\n')).flat()
 const blocked = new Set()
 for(const line of lines) {
     if(line[0] !== '#') {
@@ -16,6 +26,8 @@ let blockList = ''
 for(let key of blocked) {
     key = key.replace('server=/', '')
     key = key.replace('/', '')
+    key = key.replace('||', '')
+    key = key.replace('^', '')
     blockList += key + '\n'
 }
 
