7e62674dfe9d65b3cd489e3484b1b461d965ddec
bryopsida <bryopsida@users.noreply.github.com>
2026-05-08 06:34:24 +0000

    Squash

---
+++ buildIpv6Blocklist.mjs
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+import { writeFile } from 'fs/promises'
+import axios from 'axios'
+import ipaddr from 'ipaddr.js'
+import IPCIDR from 'ip-cidr'
+import { Address6 } from 'ip-address'
+import randomUserAgent from 'random-useragent'
+
+const feeds = [
+  'https://www.spamhaus.org/drop/dropv6.txt',
+  'https://raw.githubusercontent.com/Sekhan/TheGreatWall/master/TheGreatWall_ipv6'
+]
+
+const requests = feeds.map((url) => axios.get(url, {
+  headers: {
+    'User-Agent': randomUserAgent.getRandom()
+  }
+}))
+const responses = await Promise.all(requests)
+
+function isValid (input) {
+  try {
+    const parsedAddress = ipaddr.parse(input)
+    if (parsedAddress.kind() === 'ipv6') return true
+  } catch (_ignored) {}
+  try {
+    if (IPCIDR.isValidCIDR(input) || IPCIDR.isValidAddress(input)) {
+      const address = new Address6(input)
+      return address.isCorrect() && !address.is4()
+    }
+    return false
+  } catch (_ignored) {
+    return false
+  }
+}
+
+const ipSet = new Set()
+
+function ignoreDups (input) {
+  if (ipSet.has(input)) {
+    return false
+  } else {
+    ipSet.add(input)
+    return true
+  }
+}
+
+function normalize (input) {
+  return input.split(';')[0].trim()
+}
+
+function filterComments (input) {
+  return !input.startsWith('#') && input !== ''
+}
+
+const ips = responses.map((r) => r.data.split('\n')).flat()
+  .filter(filterComments)
+  .map(normalize)
+  .filter(isValid)
+  .filter(ignoreDups)
+  .sort()
+  .join('\n')
+
+await writeFile('ipv6.txt', ips, {
+  enconding: 'utf8'
+})
